223a52e144aab485f81ce3ad78bb2cd0544c4ecd
KCH6937 <adasddasd12@naver.com>
2022-01-29 15:26:30 +0900

    UIPickerView -> UIDatePicker로 변경

--- FriendsApp/ViewControllers/BirthViewController.swift
+++ FriendsApp/ViewControllers/BirthViewController.swift
@@ -12,9 +12,7 @@ class BirthViewController: UIViewController {
     let birthView = BirthView()
     let birthViewModel = BirthViewModel()
     
-    var year = [0]
-    var month = [0]
-    var day = [0]
+    let date = Date()
     
     override func loadView() {
         self.view = birthView
@@ -24,57 +22,31 @@ class BirthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setDate()
         setEvent()
         setPickerView()
     }
     
-    func setDate() {
-        year.removeAll()
-        month.removeAll()
-        day.removeAll()
-        
-        for i in 1900...2022 {
-            year.append(i)
-        }
-        
-        for i in 1...12 {
-            month.append(i)
-        }
-        
-        for i in 1...31 {
-            day.append(i)
-        }
+    override func viewWillAppear(_ animated: Bool) {
+        loadSavedData()
+    }
+    
+    func loadSavedData() {
         
     }
     
     func setEvent() {
-        birthViewModel.year.bind { value in
-            self.birthView.yearTextField.text = "\(value)"
-        }
-        
-        birthViewModel.month.bind { value in
-            self.birthView.monthTextField.text = "\(value)"
-        }
-        
-        birthViewModel.day.bind { value in
-            self.birthView.dayTextField.text = "\(value)"
-        }
-        
-        birthView.pickerView.delegate = self
-        birthView.pickerView.dataSource = self
-        
+        birthView.datePicker.addTarget(self, action: #selector(handleDatePicker(_:)), for: .valueChanged)
         birthView.nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
     }
     
     func setPickerView() {
-        
         let nextFieldButton = UIBarButtonItem(title: "다음", style: .plain, target: self, action: #selector(nextFieldButtonClicked))
         nextFieldButton.tintColor = .green
         
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         
         birthView.toolBar.setItems([flexSpace,nextFieldButton], animated: true)
+        
     }
     
     func checkRegex(number: String, type: String) -> Bool {
@@ -87,7 +59,7 @@ class BirthViewController: UIViewController {
         } else {
             value = "1,2"
         }
-            
+
         let regex = "^([0-9]{\(value)})$"
 
         guard number.range(of: regex, options: .regularExpression) != nil else {
@@ -117,7 +89,12 @@ class BirthViewController: UIViewController {
         
     }
     
+    @objc func handleDatePicker(_ sender: UIDatePicker) {
+        
+    }
+    
     @objc func nextButtonClicked() {
+        UserDefaults.standard.set("value", forKey: "birth")
         self.navigationController?.pushViewController(EmailViewController(), animated: true)
     }
     
@@ -138,46 +115,46 @@ class BirthViewController: UIViewController {
     
 }
 
-extension BirthViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if birthView.yearTextField.isEditing {
-            return "\(year[row])"
-        } else if birthView.monthTextField.isEditing {
-            return "\(month[row])"
-        } else if birthView.dayTextField.isEditing {
-            return "\(day[row])"
-        } else {
-            return ""
-        }
-        
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if birthView.yearTextField.isEditing {
-            return year.count
-        } else if birthView.monthTextField.isEditing {
-            return month.count
-        } else if birthView.dayTextField.isEditing {
-            return day.count
-        } else {
-            return 0
-        }
-        
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if birthView.yearTextField.isEditing {
-            birthViewModel.year.value = year[row]
-        } else if birthView.monthTextField.isEditing {
-            birthViewModel.month.value = month[row]
-        } else if birthView.dayTextField.isEditing {
-            birthViewModel.day.value = day[row]
-        }
-        
-    }
-    
-}
+//extension BirthViewController: UIDAte {
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        if birthView.yearTextField.isEditing {
+//            return "\(year[row])년"
+//        } else if birthView.monthTextField.isEditing {
+//            return "\(month[row])월"
+//        } else if birthView.dayTextField.isEditing {
+//            return "\(day[row])일"
+//        } else {
+//            return ""
+//        }
+//
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        if birthView.yearTextField.isEditing {
+//            return year.count
+//        } else if birthView.monthTextField.isEditing {
+//            return month.count
+//        } else if birthView.dayTextField.isEditing {
+//            return day.count
+//        } else {
+//            return 0
+//        }
+//
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        if birthView.yearTextField.isEditing {
+//            birthViewModel.year.value = year[row]
+//        } else if birthView.monthTextField.isEditing {
+//            birthViewModel.month.value = month[row]
+//        } else if birthView.dayTextField.isEditing {
+//            birthViewModel.day.value = day[row]
+//        }
+//
+//    }
+//
+//}
