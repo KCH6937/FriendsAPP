e913aa61a9b85aa4ed65cbdb16df1dc487b02f9b
KCH6937 <adasddasd12@naver.com>
2022-01-29 15:28:11 +0900

    홈 뷰(아직 뷰 구성전)

--- FriendsApp/ViewControllers/HomeViewController.swift
+++ FriendsApp/ViewControllers/HomeViewController.swift
@@ -12,5 +12,15 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
+        
+        removeUserDefaultsData()
     }
+    
+    func removeUserDefaultsData() {
+        UserDefaults.standard.removeObject(forKey: "nick")
+        UserDefaults.standard.removeObject(forKey: "birth")
+        UserDefaults.standard.removeObject(forKey: "email")
+        UserDefaults.standard.removeObject(forKey: "gender")
+    }
+    
 }
